263a8e648e7143203030c56d718fe23adfa2edb1
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-09-21 12:02:00 +0600

    js to login

---
+++ login-page.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('button')
+
+button.addEventListener('click', function(){
+    // console.log('hit me')
+    var loginForm = document.getElementById('login-form');
+    loginForm.style.display = "none";
+
+})
